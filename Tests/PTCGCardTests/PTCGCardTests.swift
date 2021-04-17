8b237a149ee1d3e13673b69bc02b58725bf95b83
evdwarf <eimer.aru1127@gmail.com>
2021-04-18 06:04:20 +0900

    Refactor basic energy test case

--- Tests/PTCGCardTests/PTCGCardTests.swift
+++ Tests/PTCGCardTests/PTCGCardTests.swift
@@ -3,15 +3,28 @@ import XCTest
 
 final class PTCGEnergyCardTests: XCTestCase {
 
+    let basicFireEnergyCard = PTCGBasicEnergyCard.init(at: .fire)
+    let tripleAccelerationEnergy = PTCGSpecialEnergyCard(id: "SM10-091/095-U",
+                                                         name: "トリプル加速エネルギー",
+                                                         energies: [.colorLess, .colorLess, .colorLess],
+                                                         capacity: 3)
+
     func testBasicEnergyCard() {
-        let basicFireEnergyCard = PTCGBasicEnergyCard.init(at: .fire)
         XCTAssertEqual("basic-energy-fire", basicFireEnergyCard.id)
         XCTAssertEqual(.fire, basicFireEnergyCard.energies.first)
         XCTAssertEqual("基本炎エネルギー", basicFireEnergyCard.name)
         XCTAssertEqual(1, basicFireEnergyCard.capacity)
     }
+    
+    func testSpecialEnergyCard() {
+        XCTAssertEqual("SM10-091/095-U", tripleAccelerationEnergy.id)
+        XCTAssertEqual([.colorLess, .colorLess, .colorLess], tripleAccelerationEnergy.energies)
+        XCTAssertEqual("トリプル加速エネルギー", tripleAccelerationEnergy.name)
+        XCTAssertEqual(3, tripleAccelerationEnergy.capacity)
+    }
 
     static var allTests = [
         ("testBasicEnergyCard", testBasicEnergyCard),
+        ("testSpecialEnergyCard", testSpecialEnergyCard)
     ]
 }
